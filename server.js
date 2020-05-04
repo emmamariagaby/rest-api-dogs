e41e5837a501674e45a8e5d59848644e1ab3c0f8
emmamariagaby <emmamariagaby@gmail.com>
2020-05-04 23:45:50 +0200

    dog api

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 const express = require('express')
 const app = express()
 
+// dog api 
+const dogs = [
+    {   
+        id: 1111,
+        name: "Bellman",
+        breed: "Staffordshire bullterrier",
+    },
+    {   
+        id: 2222,
+        name: "Tova",
+        breed: "Retriever mix",
+    },
+    {   
+        id: 3333,
+        name: "Love",
+        breed: "Pomeranian",
+    },
+    {   
+        id: 4444,
+        name: "Texas",
+        breed: "Dachshund",
+    },
+    {   
+        id: 5555,
+        name: "Astrid",
+        breed: "Petit brabancon",
+    },
+]
+
 // read
 app.get('/', (req, res) =>  {
     res.send('Dog api');
